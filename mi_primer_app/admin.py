2e991591c99fab3e778f69620e57d12f537bdee3
Matias Gonzalez <tanganillo@gmail.com>
2025-07-04 22:06:07 -0300

    Agregamos nuevos modelos

--- mi_primer_app/admin.py
+++ mi_primer_app/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Familiar, Curso, Estudiante
+
+regiter_models = [Familiar, Curso, Estudiante]
+
+admin.site.register(regiter_models)
--- mi_primer_app/models.py
+++ mi_primer_app/models.py
@@ -12,3 +12,27 @@ class Familiar(models.Model):
 
     def __str__(self):
         return f"{self.nombre} {self.apellido} ({self.parentesco}) - Edad: {self.edad}, Nacido el: {self.fecha_nacimiento}"
+    
+
+
+class Curso(models.Model):
+    nombre = models.CharField(max_length=100)
+    descripcion = models.TextField(blank=True, null=True)
+    duracion_semanas = models.IntegerField(default=4)
+    fecha_inicio = models.DateField()
+    activo = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.nombre
+    
+
+
+class Estudiante(models.Model):
+    nombre = models.CharField(max_length=100)
+    apellido = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+    edad = models.IntegerField()
+    fecha_inscripcion = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.nombre} {self.apellido}"
